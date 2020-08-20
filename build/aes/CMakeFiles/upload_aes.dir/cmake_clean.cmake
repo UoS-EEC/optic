e2cd54808fe5562960d61c8b45219a4b736aa522
jz8u17 <j.zhan@soton.ac.uk>
2020-08-20 15:09:45 +0100

    aesa module working

--- build/aes/CMakeFiles/upload_aes.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/upload_aes"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/upload_aes.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
