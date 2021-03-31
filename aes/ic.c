1edab2ee4b020fb36f1ef70ec7d476b0abf977cb
jz8u17 <j.zhan@soton.ac.uk>
2021-03-31 11:16:04 +0100

    arranged build files

--- aes/ic.c
+++ aes/ic.c
@@ -28,6 +28,7 @@ extern uint8_t __bssstart, __bssend;
 extern uint8_t __ramtext_low, __ramtext_high, __ramtext_loadLow;
 extern uint8_t __stack, __stackend;
 
+static void clock_init(void);
 static void gpio_init(void);
 static void adc12_init(void);
 static void comp_init(void);
@@ -58,9 +59,9 @@ fastmemcpy(uint8_t *dst, uint8_t *src, size_t len) {
 
 void __attribute__((interrupt(RESET_VECTOR), naked, used, optimize("O0")))
 iclib_boot() {
-    WDTCTL = WDTPW | WDTHOLD;              // Stop watchdog timer
-    __set_SP_register(&__bootstackend); // Boot stack
-    __bic_SR_register(GIE);                // Disable interrupts during startup
+    WDTCTL = WDTPW | WDTHOLD;            // Stop watchdog timer
+    __set_SP_register(&__bootstackend);  // Boot stack
+    __bic_SR_register(GIE);              // Disable interrupts during startup
 
     // Boot functions that are mapped to ram (most importantly fastmemcpy)
     uint8_t *dst = &__ramtext_low;
@@ -70,14 +71,14 @@ iclib_boot() {
         *dst++ = *src++;
     }
 
-    // clock_init();
+    clock_init();
     gpio_init();
     adc12_init();
     comp_init();
 
     // needRestore = 1;                    // Indicate powerup
-    P1OUT |= BIT4; // debug
-    __bis_SR_register(LPM4_bits + GIE); // Enter LPM4 with interrupts enabled
+    P1OUT |= BIT4;  // debug
+    __bis_SR_register(LPM4_bits + GIE);  // Enter LPM4 with interrupts enabled
     // Processor sleeps
     // ...
     // Processor wakes up after interrupt
@@ -86,50 +87,44 @@ iclib_boot() {
 
     // First time boot: Set SP, load data and initialize LRU
 
-    __set_SP_register(&__stackend); // Runtime stack
+    __set_SP_register(&__stackend);  // Runtime stack
 
     // load .data to RAM
     fastmemcpy(&__datastart, &__romdatastart, &__dataend - &__datastart);
 
-    int main(); // Suppress implicit decl. warning
+    int main();  // Suppress implicit decl. warning
     main();
 }
 
+static void clock_init(void) {
+    CSCTL0_H = 0xA5;  // Unlock register
+    CSCTL1 |= DCOFSEL_6;  // DCO 8MHz
+
+    // Set ACLK = VLO; SMCLK = DCO; MCLK = DCO;
+    CSCTL2 = SELA_0 + SELS_3 + SELM_3;
+
+    // ACLK: Source/1; SMCLK: Source/1; MCLK: Source/1;
+    CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;  // SMCLK = MCLK = 8 MHz
+
+    CSCTL0_H = 0x01;  // Lock Register
+}
+
 static void gpio_init(void) {
-    // Need to initialize all ports/pins to reduce power consump'n
-    P1OUT = 0; 
+    // Initialize all pins to output low to reduce power consumption
+    P1OUT = 0;
     P1DIR = 0xff;
     P2OUT = 0;
     P2DIR = 0xff;
     P3OUT = 0;
     P3DIR = 0xff;
-    P4OUT = 0; 
+    P4OUT = 0;
     P4DIR = 0xff;
     P7OUT = 0;
     P7DIR = 0xff;
     P8OUT = 0;
     P8DIR = 0xff;
-
-    /* Keep-alive signal to external comparator */
-    // P6OUT = BIT0;    // resistor enable
-    // P6REN = BIT0;    // Pull-up mode
-    // P6DIR = ~(BIT0); // All but 0 to output
-    // P6OUT = BIT0;
-    // P6DIR = 0xff;
-
-    /* Interrupts from S1 and S2 buttons */
-    // P5DIR &= ~(BIT5 | BIT6);        // All but 5,6 to output direction
-    // P5OUT |= BIT5 | BIT6;           // Pull-up resistor
-    // P5REN |= BIT5 | BIT6;           // Pull-up mode
-    // P5IES = BIT5 | BIT6;            // Falling edge
-
-    /* Interrupts from S2 button */
-    // P5DIR &= ~BIT5;          // bit 5 input direction
-    // P5OUT |= BIT5;           // Pull-up resistor
-    // P5REN |= BIT5;           // Pull-up resistor enable
-    // P5IES = BIT5;            // Falling edge
-    // P5IFG = 0;               // Clear pending interrupts
-    // P5IE = BIT5;             // Enable restore irq
+    PJOUT = 0;
+    PJDIR = 0xff;
 
     // Disable GPIO power-on default high-impedance mode
     PM5CTL0 &= ~LOCKLPM5;
@@ -157,56 +152,6 @@ static void adc12_init(void) {
                  ADC12VRSEL_1 ;             // VR+ = VREF buffered, VR- = Vss
 
     ADC12IER0 = ADC12IE0;                   // Enable ADC conv complete interrupt
-}
-
-// ADC12 interrupt service routine
-void __attribute__ ((interrupt(ADC12_B_VECTOR))) ADC12_ISR (void) {
-    switch(__even_in_range(ADC12IV, ADC12IV__ADC12RDYIFG))
-    {
-        case ADC12IV__NONE:        break;   // Vector  0:  No interrupt
-        case ADC12IV__ADC12OVIFG:  break;   // Vector  2:  ADC12MEMx Overflow
-        case ADC12IV__ADC12TOVIFG: break;   // Vector  4:  Conversion time overflow
-        case ADC12IV__ADC12HIIFG:  break;   // Vector  6:  ADC12BHI
-        case ADC12IV__ADC12LOIFG:  break;   // Vector  8:  ADC12BLO
-        case ADC12IV__ADC12INIFG:  break;   // Vector 10:  ADC12BIN
-        case ADC12IV__ADC12IFG0:            // Vector 12:  ADC12MEM0
-            adc_reading = ADC12MEM0;
-            __bic_SR_register_on_exit(LPM0_bits);
-            break;   
-        case ADC12IV__ADC12IFG1:   break;   // Vector 14:  ADC12MEM1
-        case ADC12IV__ADC12IFG2:   break;   // Vector 16:  ADC12MEM2
-        case ADC12IV__ADC12IFG3:   break;   // Vector 18:  ADC12MEM3
-        case ADC12IV__ADC12IFG4:   break;   // Vector 20:  ADC12MEM4
-        case ADC12IV__ADC12IFG5:   break;   // Vector 22:  ADC12MEM5
-        case ADC12IV__ADC12IFG6:   break;   // Vector 24:  ADC12MEM6
-        case ADC12IV__ADC12IFG7:   break;   // Vector 26:  ADC12MEM7
-        case ADC12IV__ADC12IFG8:   break;   // Vector 28:  ADC12MEM8
-        case ADC12IV__ADC12IFG9:   break;   // Vector 30:  ADC12MEM9
-        case ADC12IV__ADC12IFG10:  break;   // Vector 32:  ADC12MEM10
-        case ADC12IV__ADC12IFG11:  break;   // Vector 34:  ADC12MEM11
-        case ADC12IV__ADC12IFG12:  break;   // Vector 36:  ADC12MEM12
-        case ADC12IV__ADC12IFG13:  break;   // Vector 38:  ADC12MEM13
-        case ADC12IV__ADC12IFG14:  break;   // Vector 40:  ADC12MEM14
-        case ADC12IV__ADC12IFG15:  break;   // Vector 42:  ADC12MEM15
-        case ADC12IV__ADC12IFG16:  break;   // Vector 44:  ADC12MEM16
-        case ADC12IV__ADC12IFG17:  break;   // Vector 46:  ADC12MEM17
-        case ADC12IV__ADC12IFG18:  break;   // Vector 48:  ADC12MEM18
-        case ADC12IV__ADC12IFG19:  break;   // Vector 50:  ADC12MEM19
-        case ADC12IV__ADC12IFG20:  break;   // Vector 52:  ADC12MEM20
-        case ADC12IV__ADC12IFG21:  break;   // Vector 54:  ADC12MEM21
-        case ADC12IV__ADC12IFG22:  break;   // Vector 56:  ADC12MEM22
-        case ADC12IV__ADC12IFG23:  break;   // Vector 58:  ADC12MEM23
-        case ADC12IV__ADC12IFG24:  break;   // Vector 60:  ADC12MEM24
-        case ADC12IV__ADC12IFG25:  break;   // Vector 62:  ADC12MEM25
-        case ADC12IV__ADC12IFG26:  break;   // Vector 64:  ADC12MEM26
-        case ADC12IV__ADC12IFG27:  break;   // Vector 66:  ADC12MEM27
-        case ADC12IV__ADC12IFG28:  break;   // Vector 68:  ADC12MEM28
-        case ADC12IV__ADC12IFG29:  break;   // Vector 70:  ADC12MEM29
-        case ADC12IV__ADC12IFG30:  break;   // Vector 72:  ADC12MEM30
-        case ADC12IV__ADC12IFG31:  break;   // Vector 74:  ADC12MEM31
-        case ADC12IV__ADC12RDYIFG: break;   // Vector 76:  ADC12RDY
-        default: break;
-    }
 }
 
 static void comp_init(void) {
@@ -247,29 +192,6 @@ static void comp_init(void) {
     //          CERDYIE  ;             // Ready interrupt enabled
 
     CECTL1 |= CEON;                 // Turn On Comparator_E
-}
-
-// Comparator E interrupt service routine
-void __attribute__ ((interrupt(COMP_E_VECTOR))) Comp_ISR (void) {
-    switch(__even_in_range(CEIV, CEIV_CERDYIFG)) {
-        case CEIV_NONE: break;
-        case CEIV_CEIFG:
-            CEINT = (CEINT & ~CEIFG & ~CEIE & ~CEIIFG) | CEIIE;
-            CECTL1 |= CEMRVL;
-            __bic_SR_register_on_exit(LPM4_bits);
-            P1OUT &= ~BIT4; // debug
-            break;
-        case CEIV_CEIIFG:
-            CEINT = (CEINT & ~CEIIFG & ~CEIIE & ~CEIFG) | CEIE;
-            CECTL1 &= ~CEMRVL;
-            __bis_SR_register_on_exit(LPM4_bits | GIE);
-            P1OUT |= BIT4; // debug
-            break;
-        case CEIV_CERDYIFG:
-            CEINT &= ~CERDYIFG;
-            break;
-        default: break;
-    }
 }
 
 void atom_func_start(uint8_t func_id) {
@@ -400,3 +322,76 @@ void atom_func_end(uint8_t func_id) {
 //         P5IFG &= ~BIT5; // Clear interrupt flags
 //     } 
 // }
+
+// ADC12 interrupt service routine
+void __attribute__ ((interrupt(ADC12_B_VECTOR))) ADC12_ISR (void) {
+    switch(__even_in_range(ADC12IV, ADC12IV__ADC12RDYIFG))
+    {
+        case ADC12IV__NONE:        break;   // Vector  0:  No interrupt
+        case ADC12IV__ADC12OVIFG:  break;   // Vector  2:  ADC12MEMx Overflow
+        case ADC12IV__ADC12TOVIFG: break;   // Vector  4:  Conversion time overflow
+        case ADC12IV__ADC12HIIFG:  break;   // Vector  6:  ADC12BHI
+        case ADC12IV__ADC12LOIFG:  break;   // Vector  8:  ADC12BLO
+        case ADC12IV__ADC12INIFG:  break;   // Vector 10:  ADC12BIN
+        case ADC12IV__ADC12IFG0:            // Vector 12:  ADC12MEM0
+            adc_reading = ADC12MEM0;
+            __bic_SR_register_on_exit(LPM0_bits);
+            break;   
+        case ADC12IV__ADC12IFG1:   break;   // Vector 14:  ADC12MEM1
+        case ADC12IV__ADC12IFG2:   break;   // Vector 16:  ADC12MEM2
+        case ADC12IV__ADC12IFG3:   break;   // Vector 18:  ADC12MEM3
+        case ADC12IV__ADC12IFG4:   break;   // Vector 20:  ADC12MEM4
+        case ADC12IV__ADC12IFG5:   break;   // Vector 22:  ADC12MEM5
+        case ADC12IV__ADC12IFG6:   break;   // Vector 24:  ADC12MEM6
+        case ADC12IV__ADC12IFG7:   break;   // Vector 26:  ADC12MEM7
+        case ADC12IV__ADC12IFG8:   break;   // Vector 28:  ADC12MEM8
+        case ADC12IV__ADC12IFG9:   break;   // Vector 30:  ADC12MEM9
+        case ADC12IV__ADC12IFG10:  break;   // Vector 32:  ADC12MEM10
+        case ADC12IV__ADC12IFG11:  break;   // Vector 34:  ADC12MEM11
+        case ADC12IV__ADC12IFG12:  break;   // Vector 36:  ADC12MEM12
+        case ADC12IV__ADC12IFG13:  break;   // Vector 38:  ADC12MEM13
+        case ADC12IV__ADC12IFG14:  break;   // Vector 40:  ADC12MEM14
+        case ADC12IV__ADC12IFG15:  break;   // Vector 42:  ADC12MEM15
+        case ADC12IV__ADC12IFG16:  break;   // Vector 44:  ADC12MEM16
+        case ADC12IV__ADC12IFG17:  break;   // Vector 46:  ADC12MEM17
+        case ADC12IV__ADC12IFG18:  break;   // Vector 48:  ADC12MEM18
+        case ADC12IV__ADC12IFG19:  break;   // Vector 50:  ADC12MEM19
+        case ADC12IV__ADC12IFG20:  break;   // Vector 52:  ADC12MEM20
+        case ADC12IV__ADC12IFG21:  break;   // Vector 54:  ADC12MEM21
+        case ADC12IV__ADC12IFG22:  break;   // Vector 56:  ADC12MEM22
+        case ADC12IV__ADC12IFG23:  break;   // Vector 58:  ADC12MEM23
+        case ADC12IV__ADC12IFG24:  break;   // Vector 60:  ADC12MEM24
+        case ADC12IV__ADC12IFG25:  break;   // Vector 62:  ADC12MEM25
+        case ADC12IV__ADC12IFG26:  break;   // Vector 64:  ADC12MEM26
+        case ADC12IV__ADC12IFG27:  break;   // Vector 66:  ADC12MEM27
+        case ADC12IV__ADC12IFG28:  break;   // Vector 68:  ADC12MEM28
+        case ADC12IV__ADC12IFG29:  break;   // Vector 70:  ADC12MEM29
+        case ADC12IV__ADC12IFG30:  break;   // Vector 72:  ADC12MEM30
+        case ADC12IV__ADC12IFG31:  break;   // Vector 74:  ADC12MEM31
+        case ADC12IV__ADC12RDYIFG: break;   // Vector 76:  ADC12RDY
+        default: break;
+    }
+}
+
+// Comparator E interrupt service routine
+void __attribute__ ((interrupt(COMP_E_VECTOR))) Comp_ISR (void) {
+    switch(__even_in_range(CEIV, CEIV_CERDYIFG)) {
+        case CEIV_NONE: break;
+        case CEIV_CEIFG:
+            CEINT = (CEINT & ~CEIFG & ~CEIE & ~CEIIFG) | CEIIE;
+            CECTL1 |= CEMRVL;
+            __bic_SR_register_on_exit(LPM4_bits);
+            P1OUT &= ~BIT4; // debug
+            break;
+        case CEIV_CEIIFG:
+            CEINT = (CEINT & ~CEIIFG & ~CEIIE & ~CEIFG) | CEIE;
+            CECTL1 &= ~CEMRVL;
+            __bis_SR_register_on_exit(LPM4_bits | GIE);
+            P1OUT |= BIT4; // debug
+            break;
+        case CEIV_CERDYIFG:
+            CEINT &= ~CERDYIFG;
+            break;
+        default: break;
+    }
+}
