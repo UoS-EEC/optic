a1357561c58e87271a084cd9d9c2f07f3f417eb7
jz8u17 <j.zhan@soton.ac.uk>
2021-06-05 12:16:39 +0100

    Added test profiling

--- scripts/compe_thresholds.py
+++ scripts/compe_thresholds.py
@@ -7,4 +7,11 @@ v_max = 1.2 * 3
 print("CEREF_n : v threshold")
 for n in range(32):
     v_th = v_max * (n + 1) / 32
-    print(n, ":", "%.4f" % v_th)
+    print("%2d" % n, ":", "%.4f" % v_th)
+
+print("adc_th[32] = {")
+for n in range(32):
+    adc = 4095 * (n + 1) / 32
+    # print("%2d" % n, ":", "%4d" % adc)
+    print("%4d" % adc, ",")
+print("};")
