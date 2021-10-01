c0775fb85a2533d6c63bec503042772feed7e75b
jz8u17 <j.zhan@soton.ac.uk>
2021-10-01 16:29:18 +0100

    New simulation results

--- scripts/design_comparison/design_comparison.py
+++ scripts/design_comparison/design_comparison.py
@@ -23,7 +23,7 @@ system_capacitance = 10e-6  # 10uF
 class BaseIntermittent:
     # arr1 and arr2 should have the same len
     def __init__(self,vcc_init,arr1,arr2) -> None:
-        self.c = system_capacitance * 0.7
+        self.c = system_capacitance * 0.4
 
         self.i_draw_lpm = 10e-6
         self.i_draw_exe = 0
@@ -238,23 +238,23 @@ def main_single():
     vcc_init = 0
     t_step = 1e-6       # Simulation time step
     t = 0
-    vcc_ae = vcc_init
+    vcc_repa = vcc_init
     vcc_samoyed = vcc_init
     vcc_debs_low = vcc_init
     vcc_debs_high = vcc_init
 
     t_trace = []
-    v_trace_ae = []
+    v_trace_repa = []
     v_trace_samoyed = []
     v_trace_debs_low = []
     v_trace_debs_high = []
     t_trace.append(t)
-    v_trace_ae.append(vcc_ae)
+    v_trace_repa.append(vcc_repa)
     v_trace_samoyed.append(vcc_samoyed)
     v_trace_debs_low.append(vcc_debs_low)
     v_trace_debs_high.append(vcc_debs_high)
 
-    ae_obj = AdaptEnergy(vcc_ae, random_arr1, random_arr2)
+    repa_obj = AdaptEnergy(vcc_repa, random_arr1, random_arr2)
     samoyed_obj = Samoyed(vcc_samoyed, random_arr1, random_arr2)
     debs_low_v_task = charge_consumption(64) / system_capacitance
     debs_low_obj = DEBS(vcc_debs_low, random_arr1, random_arr2, debs_low_v_task)
@@ -263,12 +263,12 @@ def main_single():
     debs_high_obj = DEBS(vcc_debs_high, random_arr1, random_arr2, debs_high_v_task)
 
     while t < 3:  # Total simulation time in seconds
-        vcc_ae = ae_obj.update(pv_current(vcc_ae), t_step)
+        vcc_repa = repa_obj.update(pv_current(vcc_repa), t_step)
         vcc_samoyed = samoyed_obj.update(pv_current(vcc_samoyed), t_step)
         vcc_debs_low = debs_low_obj.update(pv_current(vcc_debs_low), t_step)
         vcc_debs_high = debs_high_obj.update(pv_current(vcc_debs_high), t_step)
 
-        v_trace_ae.append(vcc_ae)
+        v_trace_repa.append(vcc_repa)
         v_trace_samoyed.append(vcc_samoyed)
         v_trace_debs_low.append(vcc_debs_low)
         v_trace_debs_high.append(vcc_debs_high)
@@ -277,7 +277,7 @@ def main_single():
         t_trace.append(t)
 
     print("Number of completions, failures\n")
-    print("AdaptEnergy:", ae_obj.n_completion, ae_obj.n_failure)
+    print("AdaptEnergy:", repa_obj.n_completion, repa_obj.n_failure)
     print("Samoyed:", samoyed_obj.n_completion, samoyed_obj.n_failure)
     print("DEBS low: ", debs_low_obj.n_completion, debs_low_obj.n_failure)
     print("DEBS high: ", debs_high_obj.n_completion, debs_high_obj.n_failure)
@@ -285,7 +285,7 @@ def main_single():
     fig = plt.figure(constrained_layout=True)
     gs = gridspec.GridSpec(ncols=2, nrows=2, figure=fig)
     ax0 = fig.add_subplot(gs[0, 0])
-    ax0.plot(t_trace, v_trace_ae)
+    ax0.plot(t_trace, v_trace_repa)
     ax1 = fig.add_subplot(gs[1, 0])
     ax1.plot(t_trace, v_trace_samoyed)
     ax2 = fig.add_subplot(gs[0, 1])
@@ -297,8 +297,8 @@ def main_single():
 
 # Multi tests main
 def main_multi():
-    ae_completion =[]
-    ae_failure = []
+    repa_completion =[]
+    repa_failure = []
     samoyed_completion = []
     samoyed_failure = []
     debs_low_completion = []
@@ -328,12 +328,12 @@ def main_multi():
         vcc_init = 0
         t_step = 1e-6       # Simulation time step
         t = 0
-        vcc_ae = vcc_init
+        vcc_repa = vcc_init
         vcc_samoyed = vcc_init
         vcc_debs_low = vcc_init
         vcc_debs_high = vcc_init
 
-        ae_obj = AdaptEnergy(vcc_ae, random_arr1, random_arr2)
+        repa_obj = AdaptEnergy(vcc_repa, random_arr1, random_arr2)
         samoyed_obj = Samoyed(vcc_samoyed, random_arr1, random_arr2)
         debs_low_v_task = charge_consumption(64) / system_capacitance
         debs_low_obj = DEBS(vcc_debs_low, random_arr1, random_arr2, debs_low_v_task)
@@ -342,7 +342,7 @@ def main_multi():
         debs_high_obj = DEBS(vcc_debs_high, random_arr1, random_arr2, debs_high_v_task)
 
         while t < 2:  # Total simulation time in seconds
-            vcc_ae = ae_obj.update(pv_current(vcc_ae), t_step)
+            vcc_repa = repa_obj.update(pv_current(vcc_repa), t_step)
             vcc_samoyed = samoyed_obj.update(pv_current(vcc_samoyed), t_step)
             vcc_debs_low = debs_low_obj.update(pv_current(vcc_debs_low), t_step)
             vcc_debs_high = debs_high_obj.update(pv_current(vcc_debs_high), t_step)
@@ -350,8 +350,8 @@ def main_multi():
 
         print("Round", loop_count, "completed.")
 
-        ae_completion.append(ae_obj.n_completion)
-        ae_failure.append(ae_obj.n_failure)
+        repa_completion.append(repa_obj.n_completion)
+        repa_failure.append(repa_obj.n_failure)
         samoyed_completion.append(samoyed_obj.n_completion)
         samoyed_failure.append(samoyed_obj.n_failure)
         debs_low_completion.append(debs_low_obj.n_completion)
@@ -359,7 +359,7 @@ def main_multi():
         debs_high_completion.append(debs_high_obj.n_completion)
         debs_high_failure.append(debs_high_obj.n_failure)
 
-        del ae_obj
+        del repa_obj
         del samoyed_obj
         del debs_low_obj
         del debs_high_obj
@@ -370,10 +370,38 @@ def main_multi():
     print("(Number of failures) Mean, Max, Min")
     print(" ")
 
+    debs_low_perf_mean = sum(debs_low_completion) / len(debs_low_completion)
+    debs_low_perf_err_p = max(debs_low_completion) - debs_low_perf_mean
+    debs_low_perf_err_m = debs_low_perf_mean - min(debs_low_completion)
+    debs_low_fail_mean = sum(debs_low_failure) / len(debs_low_failure)
+    debs_low_fail_err_p = max(debs_low_failure) - debs_low_fail_mean
+    debs_low_fail_err_m = debs_low_fail_mean - min(debs_low_failure)
 
-    print("AdaptEnergy:", ae_completion, ae_failure)
-    print(sum(ae_completion) / len(ae_completion), max(ae_completion), min(ae_completion))
-    print(sum(ae_failure) / len(ae_failure), max(ae_failure), min(ae_failure))
+    samoyed_perf_mean = sum(samoyed_completion) / len(samoyed_completion)
+    samoyed_perf_err_p = max(samoyed_completion) - samoyed_perf_mean
+    samoyed_perf_err_m = samoyed_perf_mean - min(samoyed_completion)
+    samoyed_fail_mean = sum(samoyed_failure) / len(samoyed_failure)
+    samoyed_fail_err_p = max(samoyed_failure) - samoyed_fail_mean
+    samoyed_fail_err_m = samoyed_fail_mean - min(samoyed_failure)
+
+
+    debs_high_perf_mean = sum(debs_high_completion) / len(debs_high_completion)
+    debs_high_perf_err_p = max(debs_high_completion) - debs_high_perf_mean
+    debs_high_perf_err_m = debs_high_perf_mean - min(debs_high_completion)
+    debs_high_fail_mean = sum(debs_high_failure) / len(debs_high_failure)
+    debs_high_fail_err_p = max(debs_high_failure) - debs_high_fail_mean
+    debs_high_fail_err_m = debs_high_fail_mean - min(debs_high_failure)
+
+    repa_perf_mean = sum(repa_completion) / len(repa_completion)
+    repa_perf_err_p = max(repa_completion) - repa_perf_mean
+    repa_perf_err_m = repa_perf_mean - min(repa_completion)
+    repa_fail_mean = sum(repa_failure) / len(repa_failure)
+    repa_fail_err_p = max(repa_failure) - repa_fail_mean
+    repa_fail_err_m = repa_fail_mean - min(repa_failure)
+
+    print("AdaptEnergy:", repa_completion, repa_failure)
+    print(repa_perf_mean, max(repa_completion), min(repa_completion))
+    print(sum(repa_failure) / len(repa_failure), max(repa_failure), min(repa_failure))
     print("Samoyed:", samoyed_completion, samoyed_failure)
     print(sum(samoyed_completion) / len(samoyed_completion), max(samoyed_completion), min(samoyed_completion))
     print(sum(samoyed_failure) / len(samoyed_failure), max(samoyed_failure), min(samoyed_failure))
@@ -383,6 +411,17 @@ def main_multi():
     print("DEBS high:", debs_high_completion, debs_high_failure)
     print(sum(debs_high_completion) / len(debs_high_completion), max(debs_high_completion), min(debs_high_completion))
     print(sum(debs_high_failure) / len(debs_high_failure), max(debs_high_failure), min(debs_high_failure))
+
+
+    print("For csv:")
+    print(debs_low_perf_mean, ",", debs_low_perf_err_p, ",", debs_low_perf_err_m, ",",\
+samoyed_perf_mean, ",", samoyed_perf_err_p, ",", samoyed_perf_err_m, ",",\
+debs_high_perf_mean, ",", debs_high_perf_err_p, ",", debs_high_perf_err_m, ",",\
+repa_perf_mean, ",", repa_perf_err_p, ",", repa_perf_err_m, ",",\
+debs_low_fail_mean, ",", debs_low_fail_err_p, ",", debs_low_fail_err_m, ",",\
+samoyed_fail_mean, ",", samoyed_fail_err_p, ",", samoyed_fail_err_m, ",",\
+debs_high_fail_mean, ",", debs_high_fail_err_p, ",", debs_high_fail_err_m, ",",\
+repa_fail_mean, ",", repa_fail_err_p, ",", repa_fail_err_m)
 
 
 if __name__ == "__main__":
