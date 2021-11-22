4017b1940f26b05006f856803fd1dfd561eb2614
jz8u17 <j.zhan@soton.ac.uk>
2021-11-22 21:43:42 +0000

    Update the new PV curve

--- scripts/linear_regression/linear_regression.py
+++ scripts/linear_regression/linear_regression.py
@@ -7,11 +7,12 @@
 # %%
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 import csv
 xx =[]
 yy = []
-filename = 'pv_curve.csv'  # Modify filename to the relavant data
+filename = 'pv_curve_new.csv'  # Modify filename to the relavant data
 x_col = 0       # Modify index to the x column
 y_col = 1       # Modify index to the y column
 with open(filename) as csv_file:
@@ -31,6 +32,16 @@ with open(filename) as csv_file:
     print("x = ", "'" + x_col_name + "'", xx)
     print("y = ", "'" + y_col_name + "'", yy)
 
+plt.scatter(xx, yy)
+plt.ylabel('Current (uA)')
+plt.xlabel('Voltage (V)')
+# plt.show()
+
+for i in range(11):
+    xx.pop()
+    yy.pop()
+print("Last element:", xx[len(xx) - 1], yy[len(yy) - 1])
+
 x = np.array(xx).reshape((-1, 1))
 y = np.array(yy)
 
@@ -44,13 +55,14 @@ print('Slope:', model.coef_[0])
 print('Linear fit equation:', "y =", model.coef_[0], "* x +", model.intercept_)
 print('Coefficient of determination:', r_sq)
 
-
+# Another method
 print('')
 print('Manual uni-variate linear regression using normal equation:')
 sum_x = 0
 sum_x2 = 0
 sum_y = 0
 sum_xy  = 0
+size = len(xx)  # Update new size
 for i in range(size):
     sum_x += xx[i]
     sum_x2 += xx[i] * xx[i]
@@ -60,7 +72,11 @@ theta_0 = (sum_x2 * sum_y - sum_x * sum_xy) / (size * sum_x2 - sum_x * sum_x)
 theta_1 = (size * sum_xy - sum_x * sum_y) / (size * sum_x2 - sum_x * sum_x)
 print("theta_0 =", theta_0)
 print("theta_1 =", theta_1)
-print(size * sum_x2 - sum_x * sum_x)
+# print(size * sum_x2 - sum_x * sum_x)
 
+x = np.linspace(0, 4, 100)
+y = theta_0 + theta_1 * x
+plt.plot(x, y, 'r')
+plt.show()
 
 #%%
